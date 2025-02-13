6e4e1fa10f6c4465f9adc40a1d521730dbe1807b
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2025-02-13 22:03:12 +0700

    feat: brand and coupon entity

---
+++ ECommerceApplication/src/main/java/com/app/entites/Coupon.java
@@ -0,0 +1,39 @@
+package com.app.entites;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "coupons")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long couponId;
+
+	@NotBlank
+	private String couponName;
+
+    @NotBlank
+    private String couponCode;
+
+    @NotBlank
+    private double discountValue;
+
+	@OneToMany(mappedBy = "coupon", cascade =  { CascadeType.PERSIST, CascadeType.MERGE } )
+	private List<Product> products = new ArrayList<>();
+}
